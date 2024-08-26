521e7b01f619a1eade1301d8760acea58daf9f09
Melissa <mdejesus1127@gmail.com>
2024-08-26 10:17:45 -0400

    initial commit

---
+++ Unit 2 Assignments/Acostic.py
@@ -0,0 +1,6 @@
+print("Every effort counts.")
+print("Opportunities don't happen,")
+print("Great things take time,")
+print("Always strive for excellence,")
+print("Never give up on your dreams,")
+print("Rather, you create them.")
